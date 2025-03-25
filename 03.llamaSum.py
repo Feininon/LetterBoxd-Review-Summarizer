114753c0c9a692a2271135283f092aa2fe653bed
Gokulramanan <150369347+Feininon@users.noreply.github.com>
2025-03-25 15:01:06 +0530

    whole lotta changes 03.llamaSum.py

--- 03.llamaSum.py
+++ 03.llamaSum.py
@@ -1,4 +1,5 @@
 import ollama
+from star_clean import clean_reviews
 
 # Load the text data from the file
 def load_reviews(file_path):
@@ -15,7 +16,16 @@ def summarize_reviews_with_llama(text_data):
     response = ollama.chat(
         model="llama3.2",
         messages=[
-            {"role": "user", "content": f"in the following there will be some reviews and stuff identify each review and summarize every review with 250 words EVEN IF THE REVIEW IS IN SOME OTHER LANGUAGE PLEASE RESPOND IN ENGLISH: \n{text_data}"}
+            {"role": "user", 
+             "content": f'''FORMAT:
+             there will be starts which indicate the rating out of 5 and  "Watched by [USERNAME] [DATE]"
+             [REVIEW]
+             consider the ratings and reviews and summarize it within 250 words 
+             respond in english
+             do not try to find the movie name just summarize the reviews
+             EVEN IF THE REVIEW IS IN SOME OTHER LANGUAGE PLEASE RESPOND IN ENGLISH
+             here is the reviews: \n{text_data}
+             '''}
         ]
     )
     
@@ -29,11 +39,11 @@ def summarize_reviews_with_llama(text_data):
         return "No summary available."
 
 # Main code
-file_path = "the substance 12pages.txt"  # Path to your file with reviews
+file_path = "the-brutalist_reviews.txt"  # Path to your file with reviews
 text_data = load_reviews(file_path)
-
+cleaned_data = clean_reviews(text_data)
 # Call the summarizer
-summary = summarize_reviews_with_llama(text_data)
+summary = summarize_reviews_with_llama(cleaned_data)
 
 # Display the summary
 print("Summary of Reviews:\n", summary)
